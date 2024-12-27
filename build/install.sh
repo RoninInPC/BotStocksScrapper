51a2735a7deec42a9032a97089ef1128f49fae68
RoninInPC <mrjaver@mail.ru>
2024-12-27 10:22:50 +0500

    chmodded

--- build/install.sh
+++ build/install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-rm -rf /usr/local/go && tar -C /usr/local -xzf go-1.23.0.linux-arm64.tar.gz && \
+rm -rf /usr/local/go && tar -C /usr/local -xzf go-1.23.0-linux-arm64.tar.gz && \
 export PATH=$PATH:/usr/local/go/bin &&
 CGO_ENABLED=0 GOOS=linux go build -a -installsuffix cgo -o main . &&
 docker build -t bot_stock_docker . &&
