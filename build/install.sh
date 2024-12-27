b63ad4550e399ba0292ca7ed3b2293d53cb52ad3
RoninInPC <mrjaver@mail.ru>
2024-12-27 10:33:18 +0500

    изменение деплоя

--- build/install.sh
+++ build/install.sh
@@ -2,6 +2,7 @@
 
 rm -rf /usr/local/go && tar -C /usr/local -xzf go1.22.3.linux-amd64.tar.gz && \
 export PATH=$PATH:/usr/local/go/bin &&
+cd .. && cd cmd &&
 CGO_ENABLED=0 GOOS=linux go build -a -installsuffix cgo -o main . &&
-docker build -t bot_docker . &&
+docker build -t bot_stocks_docker . &&
 docker-compose up -d --build
